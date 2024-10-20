54e1b12bb4a97a73c0b1269366f24c758ff708a5
Oscar <rn24588@bristol.ac.uk>
2024-10-20 14:26:28 +0000

    bagel branched

---
+++ bagel.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// void bageler (char *arg){
+	// printf("nothing here");
+// }
+
+int main (int n, char *args[n]){
+	//printf("%d",n)
+	//char arg[10000];
+	printf("not implemented");
+	printf("print the arguments into the program \n to be donut shaped in the terminal");
+	return 0;
+}
